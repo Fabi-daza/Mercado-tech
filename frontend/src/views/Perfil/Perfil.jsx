bf361a14f7504895b26ad698c85b3c0ee24941e8
Fabian Daza <fabian.daza2020@gmail.com>
2023-12-01 23:45:20 -0300

    actualización perfil

--- frontend/src/views/Perfil/Perfil.jsx
+++ frontend/src/views/Perfil/Perfil.jsx
@@ -7,30 +7,41 @@ import './Perfil.css'
 
 const Perfil = () => {
   const { usuarios } = useContext(MyContext);
+  const userID = "1"; //usuario de prueba
+  const miUsuario = usuarios.filter(usuario => usuario.id === userID);
+  console.log(miUsuario)
+
   return (
-    <Container className="w-100 d-flex p-5" >
-      <Nav defaultActiveKey="/home" className="flex-column bg-primary sidebar">
+    <Container className="w-100 d-flex p-md-5 p-2" >
+      <Nav defaultActiveKey="/home" className="flex-column bg-primary sidebar p-md-3">
       <Nav.Link href="/MisProductos" > Publicaciones </Nav.Link>
       <Nav.Link href="/Favoritos" >Favoritos</Nav.Link>
-      <Nav.Link href="/home" >Modificar mis datos</Nav.Link>
+      <Nav.Link href="/home">Publicar producto</Nav.Link>
+      <Nav.Link href="/home">Modificar mis datos</Nav.Link>
     </Nav>
-       <Card className="d-flex border-0 p-4 justify-content-center" style={{ width: "65%" }}>
+
+    {
+      miUsuario.map((usuario) =>(
+        <Card className="d-flex border-0 p-4 justify-content-center" style={{ width: "65%" }}>
         <Card.Img
         className="imagen-perfil"
-        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={usuario.imagen}
       />
       <Card.Body>
         <Card.Text>
-          <i className="bi bi-telephone mx-3"></i>Some quick example
+          <i className="bi bi-person-circle mx-3"></i> <span>{usuario.nombre}</span>
         </Card.Text>
         <Card.Text>
-          <i className="bi bi-telephone mx-3"></i>Some quick example
+          <i className="bi bi-envelope mx-3"></i> <span>{usuario.email}</span>
         </Card.Text>
         <Card.Text>
-          <i className="bi bi-telephone mx-3"></i>Some quick example
+          <i className="bi bi-telephone mx-3"></i> <span>{usuario.telefono}</span>
         </Card.Text>
       </Card.Body>
     </Card>
+      )
+      )
+    }
     </Container>
   )
 }
